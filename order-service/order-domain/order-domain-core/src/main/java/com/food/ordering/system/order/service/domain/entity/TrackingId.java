19e6d1b275a41e5cd0ea79accc558c02732fbdf6
Franklin Castillo <franklincastillo18@gmail.com>
2023-06-27 12:25:46 -0400

    feat: Implementing Order Service domain logic using DDD - Order Aggregate Root

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/TrackingId.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.order.service.domain.entity;
+
+import com.food.ordering.system.domain.valueobject.BaseId;
+
+import java.util.UUID;
+
+public class TrackingId extends BaseId<UUID> {
+
+    public TrackingId(UUID value) {
+        super(value);
+    }
+}
